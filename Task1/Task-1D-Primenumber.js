8b53e1b6b863c94a025661c9525196d67659380a
GawaskarTechLearner <147240802+GawaskarTechLearner@users.noreply.github.com>
2023-11-02 05:26:42 +0530

    Update Task-1D-Primenumber.js

--- Task1/Task-1D-Primenumber.js
+++ Task1/Task-1D-Primenumber.js
@@ -1,3 +1,5 @@
+//Ananymous Function Prime number
+
 let data = [17,78,34,7, 32,23,15,43]; 
 const out=function (dataVal) {
        let flag = true;
@@ -25,3 +27,34 @@ const out=function (dataVal) {
         console.log(primeNum);
 }
         out(data );
+
+
+
+//IIFE Function Prime number
+
+let data = [17,78,34,7, 32,23,15,43]; 
+((dataVal) => {
+       let flag = true;
+       let primeNum =[];
+    for(let j=0;j<dataVal.length;j++)
+{
+            for (let i = 2; i <= dataVal[j]-1; i++)
+          
+                if (dataVal[j] % i == 0) {
+                    flag = false;
+                    break;
+                }
+                 
+            // Check and display output
+            if (flag == true){
+                console.log(dataVal[j] + " is prime");
+                primeNum.push(dataVal[j]);
+            }
+            else
+            {
+                console.log(dataVal[j] + " is not prime");
+                flag=true;
+            }
+        }
+        console.log(primeNum);
+})(data );
