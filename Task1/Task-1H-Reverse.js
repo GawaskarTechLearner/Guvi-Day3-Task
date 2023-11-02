0cee2dc56b966079d6c68107de17444e25756175
GawaskarTechLearner <147240802+GawaskarTechLearner@users.noreply.github.com>
2023-11-02 07:03:58 +0530

    Update Task-1H-Reverse.js

--- Task1/Task-1H-Reverse.js
+++ Task1/Task-1H-Reverse.js
@@ -23,3 +23,21 @@ let out = function(a, n, k)
 
 
 //IIFE Function
+//Ananymous Function
+let Array = [1, 2, 3, 4, 5]; 
+let N = Array.length; 
+let K = 2; 
+  
+(function(a, n, k) 
+{  
+    k = k % n; 
+      for (let i = 0; i < n; i++) { 
+        if (i < k) {            
+            console.log(a[n + i - k] + " "); 
+        } 
+        else {   
+             console.log((a[i - k]) + " "); 
+        } 
+    } 
+   
+})(Array, N, K); 
