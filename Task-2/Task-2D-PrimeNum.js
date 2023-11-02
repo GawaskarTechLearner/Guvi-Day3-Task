d3d0fbe321d6f715ad8144ce59fdefbdc87ba2a9
GawaskarTechLearner <147240802+GawaskarTechLearner@users.noreply.github.com>
2023-11-02 05:31:40 +0530

    Create Task-2D-PrimeNum.js

---
+++ Task-2/Task-2D-PrimeNum.js
@@ -0,0 +1,26 @@
+let data = [17,78,34,7, 32,23,15,43]; 
+((dataVal) => {
+       let flag = true;
+       let primeNum =[];
+    for(let j=0;j<dataVal.length;j++)
+{
+            for (let i = 2; i <= dataVal[j]-1; i++)
+          
+                if (dataVal[j] % i == 0) {
+                    flag = false;
+                    break;
+                }
+                 
+            // Check and display output
+            if (flag == true){
+                console.log(dataVal[j] + " is prime");
+                primeNum.push(dataVal[j]);
+            }
+            else
+            {
+                console.log(dataVal[j] + " is not prime");
+                flag=true;
+            }
+        }
+        console.log(primeNum);
+})(data );
