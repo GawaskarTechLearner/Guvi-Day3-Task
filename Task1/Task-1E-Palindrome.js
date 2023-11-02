1544724e6a82f16a78cbe3afad759e61ba2ff52e
GawaskarTechLearner <147240802+GawaskarTechLearner@users.noreply.github.com>
2023-11-02 05:50:59 +0530

    Create Task-1E-Palindrome.js

---
+++ Task1/Task-1E-Palindrome.js
@@ -0,0 +1,32 @@
+let data = ["gawas","appa","uncle","amma"]; 
+const out = function(dataVal) {
+      
+      
+      
+       let palin=[];
+    for(let j=0;j<dataVal.length;j++)
+{
+  
+   console.log(dataVal[j].toString());
+          const arrval= dataVal[j].toString().split('');
+           console.log(arrval);
+        const reverseval = arrval.reverse();
+        console.log(reverseval);
+        const output = reverseval.join('');
+
+          
+           
+            if (dataVal[j] == output){
+                console.log(dataVal[j] + " is palindrome");
+                palin.push(dataVal[j]);
+            }
+            else
+            {
+                console.log(dataVal[j] + " is not palindrome");
+               
+            }
+       
+        console.log(palin);
+}
+}
+out(data );
